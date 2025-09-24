45ff9e3e15b5cf217d77c717354bf77171cffd27
fish0119freelancer <timmyjuan0119@gmail.com>
2025-09-24 10:50:31 +0800

    change llm api to 4o

--- summarize_with_llm.py
+++ summarize_with_llm.py
@@ -30,7 +30,7 @@ def generate_news_summary_and_opinion(article):
 """
 
     res = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4o-mini",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.4
     )
